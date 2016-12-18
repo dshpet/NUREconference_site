cb45719013b73dc7db34bec34c9673a19dc0ec41
choovuck <nickname.qxo@gmail.com>
2016-12-18 19:04:44 +0200

    dropbox wrapper, list and add materials in main and admin page

--- js/adminPage.js
+++ js/adminPage.js
@@ -9,8 +9,45 @@ $(document).on('click', '.menuLink', function () {
 });
 
 $(document).on('click', '.addNewFile', function () {
-  var element = $(this);
+  $('#input-material').trigger('click');
+});
 
-  element.siblings('input[type=file]').trigger('click');
+function addFile(name, link) {
+	var tr   = $('<tr>');
+	var td   = $('<td>');
+	var span = $('<span class="glyphicon glyphicon-remove">');
+	span.click(function() {
+		removeFile(name);
+	});
+	
+	tr.append($('<td>' + name + '</td>'));
+	td.append(span);
+	tr.append(td);
+
+  $('#file-table-body').append(tr);
+}
+
+function removeFile(name) {
+	// is it even possible to remove files via dropbox api?
+	console.log('remove: ' + name);
+}
+
+$(document).ready(function() {
+
+	var db = new DropboxWrapper();
+
+	$('#input-material').change(function() {
+		var files = $(this).context.files;
+		
+		for (var i = 0; i < files.length; ++i) {
+			db.uploadMaterial(files[i], function(file) {
+				addFile(file.name, file.staticLink);
+			})
+		}
+	});
+
+	db.forEachMaterialFile(function(file) {
+		addFile(file.name, file.staticLink);		
+	})
 });
 
