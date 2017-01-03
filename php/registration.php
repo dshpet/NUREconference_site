6c0256dc2fd50ccd41270427818d81c3029f301f
dshpet <dshpet@gmail.com>
2017-01-03 22:57:26 +0200

    use PHPMailer lib to send messages. Attach file to message

--- php/registration.php
+++ php/registration.php
@@ -3,6 +3,7 @@ ini_set('display_errors','On');
 error_reporting(E_ALL);
 
 include "util.php";
+include "PHPMailer/class.phpmailer.php";
 
 if (!Util::isRequestMethod('POST'))
   die();
@@ -10,10 +11,11 @@ if (!Util::isRequestMethod('POST'))
 $authorNames 		= $_POST["name"];
 $title 				= $_POST["reportName"];
 $address 			= $_POST["address"];
-$email 				= $_POST["email"];
+$emailAdress		= $_POST["email"];
 $section 			= $_POST["section"];
 $participation 		= $_POST["participation"];
 $additionalThemes 	= $_POST["additionalTheme"];
+$file 				= $_POST["file"];
 
 $isStudent 			= isset($_POST["studentCheckbox"]);
 
@@ -39,7 +41,7 @@ $body .= "Секция " . $section . "\r\n";
 $body .= "Участвует " . ($participation == "och" ? "очно" : "заочно") . "\r\n";
 $body .= "Контактные данные: " . "\r\n";
 $body .= 	"\t" . $address . "\r\n";
-$body .= 	"\t" . $email . "\r\n";
+$body .= 	"\t" . $emailAdress . "\r\n";
 
 $body .= "\r\n";
 if ($isStudent)
@@ -67,10 +69,20 @@ $from    = "no-reply@sakmit.nure.ua";
 $to      = "nickname.qxo@gmail.com";
 
 $headers  = "From: $from \r\n";
-$headers .= "Reply-To: $email \r\n";
+$headers .= "Reply-To: $emailAdress \r\n";
 
 // needs SMTP configured
-mail($to, $subject, $body, $headers);
 
-Util::redirectLocal("thankYou.html");
+$email = new PHPMailer();
+$email->From      = $from;
+$email->FromName  = $authorNames;
+$email->Subject   = $subject;
+$email->Body      = $body;
+$email->AddAddress($to);
+$email->AddAttachment($file);
+
+if ($email->Send())
+	Util::redirectLocal("thankYou.html");
+else
+	echo "ERROR";
 ?>
